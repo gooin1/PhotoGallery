6af2cea8daf93779ff635edcd7583979a6525b3a
gooin <gooin@outlook.com>
2016-07-08 16:17:40 +0800

    可以动态加载Filckr的图片

--- app/src/main/java/io/github/gooin/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/io/github/gooin/photogallery/PhotoGalleryFragment.java
@@ -1,8 +1,11 @@
 package io.github.gooin.photogallery;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
@@ -38,7 +41,19 @@ public class PhotoGalleryFragment extends Fragment {
         setRetainInstance(true);
         new FetchItemsTask().execute();
 
-        mThumbnailDownloader = new ThumbnailDownloader<>();
+        Handler responseHandler = new Handler();
+        mThumbnailDownloader = new ThumbnailDownloader<>(responseHandler);
+        mThumbnailDownloader.setThumbnailDownloadListener(
+                new ThumbnailDownloader.ThumbnailDownloadListener<PhotoHolder>() {
+                    @Override
+                    public void onThumbnailDownloaded(PhotoHolder photoHolder, Bitmap bitmap) {
+                        Drawable drawable = new BitmapDrawable(getResources(), bitmap);
+                        photoHolder.bindDrawble(drawable);
+
+                    }
+                }
+        );
+
         mThumbnailDownloader.start();
         mThumbnailDownloader.getLooper();
         Log.i(TAG, "thread started");
@@ -122,6 +137,13 @@ public class PhotoGalleryFragment extends Fragment {
         super.onDestroy();
         mThumbnailDownloader.quit();
         Log.i(TAG, "thread destroyed");
+
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mThumbnailDownloader.clearQueue();
+
 
     }
 
--- app/src/main/java/io/github/gooin/photogallery/ThumbnailDownloader.java
+++ app/src/main/java/io/github/gooin/photogallery/ThumbnailDownloader.java
@@ -1,20 +1,59 @@
 package io.github.gooin.photogallery;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
 import android.os.HandlerThread;
+import android.os.Message;
 import android.util.Log;
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * Created by gooin on 2016/7/8.
  */
 public class ThumbnailDownloader<T> extends HandlerThread {
     private static final String TAG = "ThumbnailDownloader";
+    private static final int MESSAGE_DOWNLOAD = 0;
+
 
     private Boolean mHasQuit = false;
+    private Handler mRequestHandler;
+    private ConcurrentMap<T, String> mRequestMap = new ConcurrentHashMap<>();
+    private Handler mResponsedHandler;
+    private ThumbnailDownloadListener<T> mThumbnailDownloadListener;
 
 
-    public ThumbnailDownloader() {
+
+    public interface ThumbnailDownloadListener<T>{
+        void onThumbnailDownloaded(T target, Bitmap thumbnail);
+    }
+
+    public void setThumbnailDownloadListener(ThumbnailDownloadListener<T> listener) {
+        mThumbnailDownloadListener = listener;
+    }
+
+
+    public ThumbnailDownloader(Handler responseHandler) {
         super(TAG);
+        mResponsedHandler =responseHandler;
 
+    }
+
+    @Override
+    protected void onLooperPrepared() {
+        mRequestHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == MESSAGE_DOWNLOAD) {
+                    T target = (T) msg.obj;
+                    Log.i(TAG, "Got a request for URL: " + mRequestMap.get(target));
+                    handleRequest(target);
+                }
+            }
+        };
     }
 
     @Override
@@ -24,6 +63,48 @@ public class ThumbnailDownloader<T> extends HandlerThread {
     }
 
     public void queueThumbnail(T target, String url) {
-        Log.i(TAG, "queueThumbnail: Got a URL: "+url);
+        Log.i(TAG, "queueThumbnail: Got a URL: " + url);
+        if (url == null) {
+            mRequestMap.remove(target);
+        }else{
+            mRequestMap.put(target, url);
+            mRequestHandler.obtainMessage(MESSAGE_DOWNLOAD, target)
+                    .sendToTarget();
+        }
+    }
+
+    public void clearQueue() {
+        mRequestHandler.removeMessages(MESSAGE_DOWNLOAD);
+    }
+
+
+    private void handleRequest(final T target) {
+        try {
+            final String url = mRequestMap.get(target);
+
+            if (url == null) {
+                return;
+            }
+
+            byte[] bitmapBytes = new FlickFetchr().getUrlBytes(url);
+            final Bitmap bitmap = BitmapFactory
+                    .decodeByteArray(bitmapBytes, 0, bitmapBytes.length);
+            Log.i(TAG, "handleRequest: Bitmap created");
+
+            mResponsedHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    if (mRequestMap.get(target) != url || mHasQuit) {
+                        return;
+                    }
+                    mRequestMap.remove(target);
+                    mThumbnailDownloadListener.onThumbnailDownloaded(target, bitmap);
+                }
+            });
+
+
+        } catch (IOException ioe) {
+            Log.e(TAG, "handleRequest: Error downloading image", ioe);
+        }
     }
 }
